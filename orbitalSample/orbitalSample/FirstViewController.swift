345de705125b0da597671d5bd1b9c4fa4e989250
Anusree <Anusree@Divyadarsinis-MacBook-Pro.local>
2017-05-28 23:33:01 +0800

    orbitalSample with the main page

---
+++ orbitalSample/orbitalSample/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  orbitalSample
+//
+//  Created by Anusree on 27/5/17.
+//  Copyright © 2017 orbitalstaff. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
