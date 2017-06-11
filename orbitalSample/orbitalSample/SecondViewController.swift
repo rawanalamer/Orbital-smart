9a209519c9c7032a48490a40f42f2ba8b325e5e6
Anusree <Anusree@Divyadarsinis-MacBook-Pro.local>
2017-06-11 23:51:24 +0800

    QR Code scanner moved to mall navigation

--- orbitalSample/orbitalSample/SecondViewController.swift
+++ orbitalSample/orbitalSample/SecondViewController.swift
@@ -9,17 +9,25 @@
 import UIKit
 
 class SecondViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Do any additional setup after loading the view.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
+    
+    // MARK: - Navigation
+    
+    @IBAction func unwindToHomeScreen(segue: UIStoryboardSegue) {
+        dismiss(animated: true, completion: nil)
+    }
+    
 }
 
