58f1ca32aed4c7ad8de1c1e244404da01a482c6e
Rawan Alamer <rrwan@MacBook-Pro.local>
2017-06-17 05:12:48 -0400

    Changed urls in location sql

--- locations.sql
+++ locations.sql
@@ -1,29 +1,29 @@
-INSERT INTO locations (diagram)
-VALUES('<img src="/locations/location1.png">');
+INSERT INTO locations (locationId, diagram)
+VALUES('1', 'http://localhost:8080/Stores/location1.png');
 
-INSERT INTO locations (diagram)
-VALUES('<img src="/locations/location2.png>');
+INSERT INTO locations (locationId, diagram)
+VALUES('2', 'http://localhost:8080/Stores/location2.png');
 
-INSERT INTO locations ( diagram)
-VALUES('<img src="/locations/location3.png>');
+INSERT INTO locations (locationId, diagram)
+VALUES('3','http://localhost:8080/Stores/location3.png');
 
-INSERT INTO locations (diagram)
-VALUES('<img src="/locations/location4.png>');
+INSERT INTO locations (locationId, diagram)
+VALUES('4','http://localhost:8080/Stores/location4.png');
 
-INSERT INTO locations (diagram)
-VALUES('<img src="/locations/location5.png>');
+INSERT INTO locations (locationId, diagram)
+VALUES('5','http://localhost:8080/Stores/location5.png');
 
-INSERT INTO locations (diagram)
-VALUES('<img src="/locations/location6.png>');
+INSERT INTO locations (locationId, diagram)
+VALUES('6','http://localhost:8080/Stores/location6.png');
 
-INSERT INTO locations (diagram)
-VALUES('<img src="/locations/location7.png>');
+INSERT INTO locations (locationId, diagram)
+VALUES('7','http://localhost:8080/Stores/location7.png');
 
-INSERT INTO locations (diagram)
-VALUES('<img src="/locations/location8.png>');
+INSERT INTO locations (locationId, diagram)
+VALUES('8','http://localhost:8080/Stores/location8.png');
 
-INSERT INTO locations (diagram)
-VALUES('<img src="/locations/location9.png>');
+INSERT INTO locations (locationId, diagram)
+VALUES('9','http://localhost:8080/Stores/location9.png');
 
-INSERT INTO locations (diagram)
-VALUES('<img src="/locations/location10.png>');
+INSERT INTO locations (locationId, diagram)
+VALUES('10','http://localhost:8080/Stores/location10.png');
