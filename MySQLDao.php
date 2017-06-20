0ccb72d265cb2f781a2bbc590b82a38c58c9b5f4
Rawan Alamer <rrwan@MacBook-Pro.local>
2017-06-19 20:20:04 -0400

    changed MySQLDao

--- MySQLDao.php
+++ MySQLDao.php
@@ -1,33 +1,33 @@
 <?php
 class MySQLDao {
-	var $dbhost = null;
-	var $dbuser = null;
-	var $dbpass = null;
-	var $conn = null;
-	var $dbname = null;
-	var $result = null;
 
-	function __construct() {
-		$this->dbhost = Conn::$dbhost;
-		$this->dbuser = Conn::$dbuser;
-		$this->dbpass = Conn::$dbpass;
-		$this->dbname = Conn::$dbname;
-	}
+    var $dbhost = null;
+    var $dbuser = null;
+    var $dbpass = null;
+    var $conn = null;
+    var $dbname = null;
+    var $result = null;
+    function __construct($dbhost, $dbuser, $dbpassword, $dbname) {
+        $this->dbhost = $dbhost;
+        $this->dbuser = $dbuser;
+        $this->dbpass = $dbpassword;
+        $this->dbname = $dbname;
+    }
 
-	public function openConnection() {
-		$this->conn = new mysqli($this->dbhost, $this->dbuser, $this->dbpass, $this->dbname);
-		if (mysqli_connect_errno())
-		echo new Exception("Could not establish connection with database");
-	}
 
-	public function getConnection() {
-		return $this->conn;
-	}
+    public function openConnection() {
+        $this->conn = new mysqli($this->dbhost, $this->dbuser, $this->dbpass, $this->dbname);
+        if (mysqli_connect_errno())
+            throw new Exception("Could not establish connection with database");
+        $this->conn->set_charset("utf8");
+    }
 
-	public function closeConnection() {
-		if ($this->conn != null)
-		$this->conn->close();
-	}
+    public function closeConnection() {
+        if ($this->conn != null)
+            $this->conn->close();
+    }
+
+
 
 	public function searchStore($name){
 		$returnValue =  array();
@@ -39,7 +39,7 @@ class MySQLDao {
         $result->bind_param("ssi", $name);
         $result->execute();
         $result1 = $result->get_result();
-        
+
         while($myrow = $result->fetch_assoc())
         {
             $returnValue[] = $myrow;
