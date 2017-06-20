d6efcedc4dc42d4ad49981fb3570bfc286766dd2
Rawan Alamer <rrwan@MacBook-Pro.local>
2017-06-19 23:36:14 -0400

    insertions to directions table (incomplete)

---
+++ directions.sql
@@ -0,0 +1,210 @@
+
+INSERT INTO directions (locationId, storeId, directions, diagram)
+VALUES('1', '1', 'http://localhost:8080/directions/location1_toys''r''us');
+INSERT INTO directions (locationId, storeId, directions, diagram)
+VALUES('1', '2', 'http://localhost:8080/directions/location1_boostjuice');
+INSERT INTO directions (locationId, storeId, directions, diagram)
+VALUES('1', '3', 'http://localhost:8080/directions/location1_challenger');
+INSERT INTO directions (locationId, storeId, directions, diagram)
+VALUES('1', '4', 'http://localhost:8080/directions/location1_dental');
+INSERT INTO directions (locationId, storeId, directions, diagram)
+VALUES('1', '5', 'http://localhost:8080/directions/location1_factory');
+INSERT INTO directions (locationId, storeId, directions, diagram)
+VALUES('1', '6', 'http://localhost:8080/directions/location1_g2000');
+INSERT INTO directions (locationId, storeId, directions, diagram)
+VALUES('1','7', 'http://localhost:8080/directions/location1_goldenvillage');
+INSERT INTO directions (locationId, storeId, directions, diagram)
+VALUES('1', '8', 'http://localhost:8080/directions/location1_lego');
+INSERT INTO directions (locationId, storeId, directions, diagram)
+VALUES('1', '9', 'http://localhost:8080/directions/location1_longjohn');
+INSERT INTO directions (locationId, storeId, directions, diagram)
+VALUES('1', '10', 'http://localhost:8080/directions/location1_ripcurl');
+
+INSERT INTO directions (locationId, storeId, directions, diagram)
+VALUES('2', '1', 'http://localhost:8080/directions/location2_toys''r''us');
+INSERT INTO directions (locationId, storeId, directions, diagram)
+VALUES('2', '2', 'http://localhost:8080/directions/location2_boostjuice');
+INSERT INTO directions (locationId, storeId, directions, diagram)
+VALUES('2', '3', 'http://localhost:8080/directions/location2_challenger');
+INSERT INTO directions (locationId, storeId, directions, diagram)
+VALUES('2', '4', 'http://localhost:8080/directions/location2_dental');
+INSERT INTO directions (locationId, storeId, directions, diagram)
+VALUES('2', '5', 'http://localhost:8080/directions/location2_factorie');
+INSERT INTO directions (locationId, storeId, directions, diagram)
+VALUES('2', '6', 'http://localhost:8080/directions/location2_g2000');
+INSERT INTO directions (locationId, storeId, directions, diagram)
+VALUES('2','7', 'http://localhost:8080/directions/location2_goldenvillage');
+INSERT INTO directions (locationId, storeId, directions, diagram)
+VALUES('2', '8', 'http://localhost:8080/directions/location2_lego');
+INSERT INTO directions (locationId, storeId, directions, diagram)
+VALUES('2', '9', 'http://localhost:8080/directions/location2_longjohn');
+INSERT INTO directions (locationId, storeId, directions, diagram)
+VALUES('2', '10', 'http://localhost:8080/directions/location2_rip');
+
+INSERT INTO directions (locationId, storeId, directions, diagram)
+VALUES('3', '1', 'http://localhost:8080/directions/location3_toys''r''us');
+INSERT INTO directions (locationId, storeId, directions, diagram)
+VALUES('3', '2', 'http://localhost:8080/directions/location3_boostjuice');
+INSERT INTO directions (locationId, storeId, directions, diagram)
+VALUES('3', '3', 'http://localhost:8080/directions/location3_challenger');
+INSERT INTO directions (locationId, storeId, directions, diagram)
+VALUES('3', '4', 'http://localhost:8080/directions/location3_dental');
+INSERT INTO directions (locationId, storeId, directions, diagram)
+VALUES('3', '5', 'http://localhost:8080/directions/location3_factorie');
+INSERT INTO directions (locationId, storeId, directions, diagram)
+VALUES('3', '6', 'http://localhost:8080/directions/location3_g2000');
+INSERT INTO directions (locationId, storeId, directions, diagram)
+VALUES('3','7', 'http://localhost:8080/directions/location3_goldenvillage');
+INSERT INTO directions (locationId, storeId, directions, diagram)
+VALUES('3', '8', 'http://localhost:8080/directions/location3_lego');
+INSERT INTO directions (locationId, storeId, directions, diagram)
+VALUES('3', '9', 'http://localhost:8080/directions/location3_long');
+INSERT INTO directions (locationId, storeId, directions, diagram)
+VALUES('3', '10', 'http://localhost:8080/directions/location3_ripcurl');
+
+INSERT INTO directions (locationId, storeId, directions, diagram)
+VALUES('4', '1', 'http://localhost:8080/directions/location4_toys''r''us');
+INSERT INTO directions (locationId, storeId, directions, diagram)
+VALUES('4', '2', 'http://localhost:8080/directions/location4_boostjuice');
+INSERT INTO directions (locationId, storeId, directions, diagram)
+VALUES('4', '3', 'http://localhost:8080/directions/location4_challenger');
+INSERT INTO directions (locationId, storeId, directions, diagram)
+VALUES('4', '4', 'http://localhost:8080/directions/location4_dental');
+INSERT INTO directions (locationId, storeId, directions, diagram)
+VALUES('4', '5', 'http://localhost:8080/directions/location4_factorie');
+INSERT INTO directions (locationId, storeId, directions, diagram)
+VALUES('4', '6', 'http://localhost:8080/directions/location4_g2000');
+INSERT INTO directions (locationId, storeId, directions, diagram)
+VALUES('4','7', 'http://localhost:8080/directions/location4_goldenvillage');
+INSERT INTO directions (locationId, storeId, directions, diagram)
+VALUES('4', '8', 'http://localhost:8080/directions/location4_lego');
+INSERT INTO directions (locationId, storeId, directions, diagram)
+VALUES('4', '9', 'http://localhost:8080/directions/location4_longjohn');
+INSERT INTO directions (locationId, storeId, directions, diagram)
+VALUES('4', '10', 'http://localhost:8080/directions/location4_ripcurl');
+
+INSERT INTO directions (locationId, storeId, directions, diagram)
+VALUES('5', '1', 'http://localhost:8080/directions/location5_toys''r''us');
+INSERT INTO directions (locationId, storeId, directions, diagram)
+VALUES('5', '2', 'http://localhost:8080/directions/location5_boostjuice');
+INSERT INTO directions (locationId, storeId, directions, diagram)
+VALUES('5', '3', 'http://localhost:8080/directions/location5_challenger');
+INSERT INTO directions (locationId, storeId, directions, diagram)
+VALUES('5', '4', 'http://localhost:8080/directions/location5_dental');
+INSERT INTO directions (locationId, storeId, directions, diagram)
+VALUES('5', '5', 'http://localhost:8080/directions/location5_factorie');
+INSERT INTO directions (locationId, storeId, directions, diagram)
+VALUES('5', '6', 'http://localhost:8080/directions/location5_g2000');
+INSERT INTO directions (locationId, storeId, directions, diagram)
+VALUES('5','7', 'http://localhost:8080/directions/location5_goldenvillage');
+INSERT INTO directions (locationId, storeId, directions, diagram)
+VALUES('5', '8', 'http://localhost:8080/directions/location5_lego');
+INSERT INTO directions (locationId, storeId, directions, diagram)
+VALUES('5', '9', 'http://localhost:8080/directions/location5_longjohn');
+INSERT INTO directions (locationId, storeId, directions, diagram)
+VALUES('5', '10', 'http://localhost:8080/directions/location5_ripcurl');
+
+INSERT INTO directions (locationId, storeId, directions, diagram)
+VALUES('6', '1', 'http://localhost:8080/directions/location6_toys''r''us');
+INSERT INTO directions (locationId, storeId, directions, diagram)
+VALUES('6', '2', 'http://localhost:8080/directions/location6_boostjuice');
+INSERT INTO directions (locationId, storeId, directions, diagram)
+VALUES('6', '3', 'http://localhost:8080/directions/location6_challenger');
+INSERT INTO directions (locationId, storeId, directions, diagram)
+VALUES('6', '4', 'http://localhost:8080/directions/location6_dental');
+INSERT INTO directions (locationId, storeId, directions, diagram)
+VALUES('6', '5', 'http://localhost:8080/directions/location6_factorie');
+INSERT INTO directions (locationId, storeId, directions, diagram)
+VALUES('6', '6', 'http://localhost:8080/directions/location6_g2000');
+INSERT INTO directions (locationId, storeId, directions, diagram)
+VALUES('6','7', 'http://localhost:8080/directions/location6_goldenvillage');
+INSERT INTO directions (locationId, storeId, directions, diagram)
+VALUES('6', '8', 'http://localhost:8080/directions/location6_lego');
+INSERT INTO directions (locationId, storeId, directions, diagram)
+VALUES('6', '9', 'http://localhost:8080/directions/location6_longjohn');
+INSERT INTO directions (locationId, storeId, directions, diagram)
+VALUES('6', '10', 'http://localhost:8080/directions/location6_ripcurl');
+
+INSERT INTO directions (locationId, storeId, directions, diagram)
+VALUES('7', '1', 'http://localhost:8080/directions/location7_toys''r''us');
+INSERT INTO directions (locationId, storeId, directions, diagram)
+VALUES('7', '2', 'http://localhost:8080/directions/location7_boostjuice');
+INSERT INTO directions (locationId, storeId, directions, diagram)
+VALUES('7', '3', 'http://localhost:8080/directions/location7_challenger');
+INSERT INTO directions (locationId, storeId, directions, diagram)
+VALUES('7', '4', 'http://localhost:8080/directions/location7_dental');
+INSERT INTO directions (locationId, storeId, directions, diagram)
+VALUES('7', '5', 'http://localhost:8080/directions/location7_factorie');
+INSERT INTO directions (locationId, storeId, directions, diagram)
+VALUES('7', '6', 'http://localhost:8080/directions/location7_g2000');
+INSERT INTO directions (locationId, storeId, directions, diagram)
+VALUES('7','7', 'http://localhost:8080/directions/location7_goldenvillage');
+INSERT INTO directions (locationId, storeId, directions, diagram)
+VALUES('7', '8', 'http://localhost:8080/directions/location7_lego');
+INSERT INTO directions (locationId, storeId, directions, diagram)
+VALUES('7', '9', 'http://localhost:8080/directions/location7_longjohn');
+INSERT INTO directions (locationId, storeId, directions, diagram)
+VALUES('7', '10', 'http://localhost:8080/directions/location7_ripcurl');
+
+INSERT INTO directions (locationId, storeId, directions, diagram)
+VALUES('8', '1', 'http://localhost:8080/directions/location8_toys''r''us');
+INSERT INTO directions (locationId, storeId, directions, diagram)
+VALUES('8', '2', 'http://localhost:8080/directions/location8_boostjuice');
+INSERT INTO directions (locationId, storeId, directions, diagram)
+VALUES('8', '3', 'http://localhost:8080/directions/location8_challenger');
+INSERT INTO directions (locationId, storeId, directions, diagram)
+VALUES('8', '4', 'http://localhost:8080/directions/location8_dental');
+INSERT INTO directions (locationId, storeId, directions, diagram)
+VALUES('8', '5', 'http://localhost:8080/directions/location8_factorie');
+INSERT INTO directions (locationId, storeId, directions, diagram)
+VALUES('8', '6', 'http://localhost:8080/directions/location8_g2000');
+INSERT INTO directions (locationId, storeId, directions, diagram)
+VALUES('8','7', 'http://localhost:8080/directions/location8_goldenvillage');
+INSERT INTO directions (locationId, storeId, directions, diagram)
+VALUES('8', '8', 'http://localhost:8080/directions/location8_lego');
+INSERT INTO directions (locationId, storeId, directions, diagram)
+VALUES('8', '9', 'http://localhost:8080/directions/location8_longjohn');
+INSERT INTO directions (locationId, storeId, directions, diagram)
+VALUES('8', '10', 'http://localhost:8080/directions/location8_ripcurl');
+
+INSERT INTO directions (locationId, storeId, directions, diagram)
+VALUES('9', '1', 'http://localhost:8080/directions/location9_toys''r''us');
+INSERT INTO directions (locationId, storeId, directions, diagram)
+VALUES('9', '2', 'http://localhost:8080/directions/location9_boostjuice');
+INSERT INTO directions (locationId, storeId, directions, diagram)
+VALUES('9', '3', 'http://localhost:8080/directions/location9_challenger');
+INSERT INTO directions (locationId, storeId, directions, diagram)
+VALUES('9', '4', 'http://localhost:8080/directions/location9_dental');
+INSERT INTO directions (locationId, storeId, directions, diagram)
+VALUES('9', '5', 'http://localhost:8080/directions/location9_factorie');
+INSERT INTO directions (locationId, storeId, directions, diagram)
+VALUES('9', '6', 'http://localhost:8080/directions/location9_g2000');
+INSERT INTO directions (locationId, storeId, directions, diagram)
+VALUES('9','7', 'http://localhost:8080/directions/location9_goldenvillage');
+INSERT INTO directions (locationId, storeId, directions, diagram)
+VALUES('9', '8', 'http://localhost:8080/directions/location9_lego');
+INSERT INTO directions (locationId, storeId, directions, diagram)
+VALUES('9', '9', 'http://localhost:8080/directions/location9_longjohn');
+INSERT INTO directions (locationId, storeId, directions, diagram)
+VALUES('9', '10', 'http://localhost:8080/directions/location9_ripcurl');
+
+INSERT INTO directions (locationId, storeId, directions, diagram)
+VALUES('10', '1', 'http://localhost:8080/directions/location10_toys''r''us');
+INSERT INTO directions (locationId, storeId, directions, diagram)
+VALUES('10', '2', 'http://localhost:8080/directions/location10_boostjuice');
+INSERT INTO directions (locationId, storeId, directions, diagram)
+VALUES('10', '3', 'http://localhost:8080/directions/location10_challenger');
+INSERT INTO directions (locationId, storeId, directions, diagram)
+VALUES('10', '4', 'http://localhost:8080/directions/location10_dental');
+INSERT INTO directions (locationId, storeId, directions, diagram)
+VALUES('10', '5', 'http://localhost:8080/directions/location10_factorie');
+INSERT INTO directions (locationId, storeId, directions, diagram)
+VALUES('10', '6', 'http://localhost:8080/directions/location10_g2000');
+INSERT INTO directions (locationId, storeId, directions, diagram)
+VALUES('10','7', 'http://localhost:8080/directions/location10_goldenvillage');
+INSERT INTO directions (locationId, storeId, directions, diagram)
+VALUES('10', '8', 'http://localhost:8080/directions/location10_lego');
+INSERT INTO directions (locationId, storeId, directions, diagram)
+VALUES('10', '9', 'http://localhost:8080/directions/location10_longjohn');
+INSERT INTO directions (locationId, storeId, directions, diagram)
+VALUES('10', '10', 'http://localhost:8080/directions/location10_ripcurl');
