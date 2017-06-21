4c65abd04bae5d123d1e1ec348fe358bfc9d6d9c
Anusree <Anusree@Divyadarsinis-MacBook-Pro.local>
2017-06-21 23:32:37 +0800

    changed the constraints and updated qr view controller

--- orbitalSample/orbitalSample/QRScannerController.swift
+++ orbitalSample/orbitalSample/QRScannerController.swift
@@ -18,6 +18,8 @@ class QRScannerController: UIViewController, AVCaptureMetadataOutputObjectsDeleg
     var videoPreviewLayer:AVCaptureVideoPreviewLayer?
     var qrCodeFrameView:UIView?
     
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -102,16 +104,6 @@ class QRScannerController: UIViewController, AVCaptureMetadataOutputObjectsDeleg
         }
     }
     
-    
-    /*
-     // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destinationViewController.
-     // Pass the selected object to the new view controller.
-     }
-     */
     
 }
 
