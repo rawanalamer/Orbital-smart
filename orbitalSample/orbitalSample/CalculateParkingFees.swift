55f06b3a101b38d71873cfdf0406d32883e2c177
Anusree <Anusree@Divyadarsinis-MacBook-Pro.local>
2017-07-02 23:01:12 +0800

    added view controller for calculateParkingFees

---
+++ orbitalSample/orbitalSample/CalculateParkingFees.swift
@@ -0,0 +1,26 @@
+//
+//  CalculateParkingFees.swift
+//  orbitalSample
+//
+//  Created by Anusree on 2/7/17.
+//  Copyright © 2017 orbitalstaff. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CalculateParkingFees: UIViewController{
+    
+    override func viewDidLoad() {
+        
+        let methodStart = Date()
+        
+        let methodFinish = Date()
+        let executionTime = methodFinish.timeIntervalSince(methodStart)
+        
+        print("Execution time: \(executionTime)")
+        
+        
+    }
+
+}
