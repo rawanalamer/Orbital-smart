ad56d2dbdf0be3492b31db0476ce99ec74ef2ec2
Anusree <Anusree@Divyadarsinis-MacBook-Pro.local>
2017-07-02 14:47:50 +0800

    changed the folder from Stores to locations

--- carparks.sql
+++ carparks.sql
@@ -1,2 +1,2 @@
 INSERT INTO carparks (carparkId, zone, diagram)
-VALUES('c1', 'A', 'http://localhost:8080/Stores/c1.png');
+VALUES('c1', 'A', 'http://localhost:8080/locations/c1.png');
--- locations.sql
+++ locations.sql
@@ -1,29 +1,29 @@
 INSERT INTO locations (locationId, diagram)
-VALUES('1', 'http://localhost:8080/Stores/location1.png');
+VALUES('1', 'http://localhost:8080/locations/location1.png');
 
 INSERT INTO locations (locationId, diagram)
-VALUES('2', 'http://localhost:8080/Stores/location2.png');
+VALUES('2', 'http://localhost:8080/locations/location2.png');
 
 INSERT INTO locations (locationId, diagram)
-VALUES('3','http://localhost:8080/Stores/location3.png');
+VALUES('3','http://localhost:8080/locations/location3.png');
 
 INSERT INTO locations (locationId, diagram)
-VALUES('4','http://localhost:8080/Stores/location4.png');
+VALUES('4','http://localhost:8080/locations/location4.png');
 
 INSERT INTO locations (locationId, diagram)
-VALUES('5','http://localhost:8080/Stores/location5.png');
+VALUES('5','http://localhost:8080/locations/location5.png');
 
 INSERT INTO locations (locationId, diagram)
-VALUES('6','http://localhost:8080/Stores/location6.png');
+VALUES('6','http://localhost:8080/locations/location6.png');
 
 INSERT INTO locations (locationId, diagram)
-VALUES('7','http://localhost:8080/Stores/location7.png');
+VALUES('7','http://localhost:8080/locations/location7.png');
 
 INSERT INTO locations (locationId, diagram)
-VALUES('8','http://localhost:8080/Stores/location8.png');
+VALUES('8','http://localhost:8080/locations/location8.png');
 
 INSERT INTO locations (locationId, diagram)
-VALUES('9','http://localhost:8080/Stores/location9.png');
+VALUES('9','http://localhost:8080/locations/location9.png');
 
 INSERT INTO locations (locationId, diagram)
-VALUES('10','http://localhost:8080/Stores/location10.png');
+VALUES('10','http://localhost:8080/locations/location10.png');
