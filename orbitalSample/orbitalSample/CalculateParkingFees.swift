5c8e2f5bab00f2b3d22f6e0935f5b97112efca9d
Anusree <Anusree@Divyadarsinis-MacBook-Pro.local>
2017-07-04 22:54:28 +0800

    updated calculateParkingFees VC

--- orbitalSample/orbitalSample/CalculateParkingFees.swift
+++ orbitalSample/orbitalSample/CalculateParkingFees.swift
@@ -11,12 +11,23 @@ import UIKit
 
 class CalculateParkingFees: UIViewController{
     
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var fareLabel: UILabel!
+
     override func viewDidLoad() {
         
         let methodStart = Date()
         
         let methodFinish = Date()
         let executionTime = methodFinish.timeIntervalSince(methodStart)
+
+        
+        timeLabel.text = "\(executionTime)"
+        
+        //$1.20 for 1st hr, $0.60 for next subsequent 30min from 7am to 6pm
+        
+        fareLabel.text = "$0.00"
+        
         
         print("Execution time: \(executionTime)")
         
