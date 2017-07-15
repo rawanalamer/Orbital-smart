1c25b67413e948d2c5596b53a1cf65fa6b6f0b9e
Anusree <anusree0614@gmail.com>
2017-07-15 18:42:37 +0800

    calculation done

--- orbitalSample/orbitalSample/CalculateParkingFees.swift
+++ orbitalSample/orbitalSample/CalculateParkingFees.swift
@@ -11,27 +11,48 @@ import UIKit
 
 class CalculateParkingFees: UIViewController{
     
-    @IBOutlet weak var timeLabel: UILabel!
+
+    @IBOutlet weak var textLabel: UILabel!
     @IBOutlet weak var fareLabel: UILabel!
+    var parkedTime: NSDate!
+    var totalFare: Double!
+    
+    var carpark: SaveLocation1ViewController = SaveLocation1ViewController(nibName: nil, bundle: nil)
+    
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
 
     override func viewDidLoad() {
+        let currentTime = NSDate()
+        parkedTime = carpark.getTime()
+        let interval = currentTime.timeIntervalSince(parkedTime as Date)
+        print(interval)
+        totalFare = (interval/1800)*1.00
+        totalFare = Double(round(100*totalFare)/100)
+        print(totalFare)
         
-        let methodStart = Date()
+        fareLabel.text = "$\(totalFare!)"
         
-        let methodFinish = Date()
-        let executionTime = methodFinish.timeIntervalSince(methodStart)
 
         
-        timeLabel.text = "\(executionTime)"
-        
-        //$1.20 for 1st hr, $0.60 for next subsequent 30min from 7am to 6pm
-        
-        fareLabel.text = "$0.00"
-        
-        
-        print("Execution time: \(executionTime)")
-        
-        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func displayAlertMessage(userMessage: String)
+    {
+        let myAlert = UIAlertController(title: "Alert", message: userMessage, preferredStyle: UIAlertControllerStyle.alert);
+        let okAction =  UIAlertAction(title: "Ok", style: UIAlertActionStyle.default, handler: nil)
+        myAlert.addAction(okAction);
+        self.present(myAlert, animated: true, completion: nil)
     }
 
 }
